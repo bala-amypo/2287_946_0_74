99fba582f1598d281de27c5114714efe55c7a74a
bala-amypo <bala@amypo.in>
2025-12-17 06:08:39 +0000

    User commit

--- src/main/java/com/example/demo/service/implement/Stuserve.java
+++ src/main/java/com/example/demo/service/implement/Stuserve.java
@@ -2,23 +2,18 @@ package com.example.demo.service;
 // import java.util.HashMap;
 import java.util.List;
 // import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.entity.Student;
-
 import repository.Sturepository;
-
 @Service
-public class Stuserve {
+public interface Stuserve {
 
-   
     @Autowired
     Sturepository repo;
     public Student createData (Student stu)
     {
         return repo.save(stu);
-
     }
     public List<Integer> fetch() {
         return repo.findAll();
